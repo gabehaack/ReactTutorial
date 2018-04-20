1efeddc5d8621736415ed172f65af8a3d99a1dcd
Gabe Haack <ghaack@bluebeam.com>
2018-04-19 17:25:36 -0700

    Finished Getting Started, ready for Implementing Kanban

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
+import classnames from 'classnames';
 
-export default ({ editing, value, onEdit, ...props }) => {
+export default ({ editing, value, onEdit, className, ...props }) => {
   if (editing) {
-    return <Edit value={value} onEdit={onEdit} {...props} />;
+    return (
+      <Edit className={className} value={value} onEdit={onEdit} {...props} />
+    );
   }
 
-  return <span {...props}>value: {value}</span>;
+  return (
+    <span className={classnames('value', className)} {...props}>
+      {value}
+    </span>
+  );
 };
 
-const Edit = ({ onEdit = () => {}, value, ...props }) => (
-  <div onClick={onEdit} {...props}>
-    <span>edit: {value}</span>
-  </div>
-);
+class Edit extends React.Component {
+  render() {
+    const { className, value, onEdit, ...props } = this.props;
+
+    return (
+      <input
+        type="text"
+        className={classnames('edit', className)}
+        autoFocus={true}
+        defaultValue={value}
+        onBlur={this.finishEdit}
+        onKeyPress={this.checkEnter}
+        {...props}
+      />
+    );
+  }
+
+  checkEnter = e => {
+    if (e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+
+  finishEdit = e => {
+    const value = e.target.value;
+
+    if (this.props.onEdit) {
+      this.props.onEdit(value);
+    }
+  };
+}
